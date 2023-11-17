1ec4f9cc960aaf4d64bcc43d8be913e92c8901fa
NektoPD <143543654+NektoPD@users.noreply.github.com>
2023-11-17 22:55:23 +0300

    Update EnemySpawner.cs

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -22,6 +22,8 @@ public class RandomEnemySpawner : MonoBehaviour
             {
                 int randomSpawnPoint = Random.Range(0, _spawnPoints.Length);
                 GameObject newEnemy = Instantiate(_enemy, _spawnPoints[randomSpawnPoint].position, Quaternion.identity);
+                
+                SetEnemyDirecton(newEnemy);
 
                 _count--;
                 _spawnRunningTime = _spawnInterval;
@@ -31,5 +33,10 @@ public class RandomEnemySpawner : MonoBehaviour
                 _spawnRunningTime -= Time.deltaTime;
             }
         }
+    }
+
+    private void SetEnemyDirecton(GameObject enemy)
+    {
+        enemy.transform.Translate(Vector2.right);
     }
 }
